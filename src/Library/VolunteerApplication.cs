9f16b482de4f6640d72c078732a68bb3b27c887a
Phil Baines <phil.baines@wubbleyew.com>
2013-06-27 16:22:44 +0100

    Adding first few fields to VolunteerApplication class

--- src/Library/VolunteerApplication.cs
+++ src/Library/VolunteerApplication.cs
@@ -11,5 +11,49 @@ namespace RbcVolunteerApplications.Library
 	public class VolunteerApplication
 	{
 		
+		public VolunteerApplication(){}
+		
+		#region Properties
+		
+		public ApplicationKind ApplicationKind { get; set; }
+		
+		public FormsOfService FormsOfService { get; set; }
+		
+		public string LastName { get; set; }
+		
+		public string FirstName { get; set; }
+		
+		public string MiddleName { get; set; }
+		
+		public Gender Gender { get; set; }
+		
+		public DateTime DateOfBirth { get; set; }
+		
+		public DateTime DateOfBaptism { get; set; }
+		
+		#endregion
+		
+	}
+	
+	public enum ApplicationKind
+	{
+		NoneSpecified = 0,
+		NewApplication = 1,
+		UpdateOfPersonalData = 2
+	}
+	
+	[Flags]
+	public enum FormsOfService
+	{
+		NoneSpecified = 0,
+		HallConstruction = 1,
+		DisasterRelief = 2
+	}
+	
+	public enum Gender
+	{
+		NoneSpecified = 0,
+		Male = 1,
+		Femail = 2
 	}
 }
