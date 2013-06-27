7570e91cd2b0a9e1702a09dca55421b45e8d694a
Phil Baines <phil.baines@wubbleyew.com>
2013-06-27 16:07:11 +0100

    Adding RbcVolunteerApplications.Library and empty VolunteerApplication class

---
+++ src/Library/VolunteerApplication.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace RbcVolunteerApplications.Library
+{
+	/// <summary>
+	///  A representation of the RBC Volunteer Application form.
+	///  Based on S-82-E Bi 5/12
+	/// </summary>
+	public class VolunteerApplication
+	{
+		
+	}
+}
