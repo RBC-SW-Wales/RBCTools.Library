0a2dfc4cfafee43966c6e8a3888e7d9710102363
Phil Baines <phil.baines@wubbleyew.com>
2013-09-07 17:35:03 +0100

    Add S82Reader class and test functionality for failures.

--- src/Importer/Program.cs
+++ src/Importer/Program.cs
@@ -1,17 +1,61 @@
 ﻿
 using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using RbcVolunteerApplications.Library;
 
 namespace RbcVolunteerApplications.Importer
 {
 	class Program
 	{
+		[STAThread]
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World! I don't do any yet. But I will.");
+			Console.WriteLine("First you will need to choose the S82 PDF files that you wish to import.");
+			Console.WriteLine("Press any key to continue to select files...");
+			Console.ReadKey(true);
 			
-			// TODO: Implement Functionality Here
+			var failures = new Dictionary<string, List<string>>();
 			
-			Console.Write("Press any key to continue . . . ");
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Multiselect = true;
+			dlg.Title = "Choose S82 PDF files";
+			dlg.Filter = "PDF Files|*.pdf";
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				foreach(string str in dlg.FileNames)
+				{
+					FileInfo file = new FileInfo(str);
+					Console.WriteLine(string.Format("Importing file: {0} (not really, soon though!)", file.Name));
+					var reader = new S82Reader(str);
+					if(!reader.ReadSuccessful)
+					{
+						failures.Add(file.Name, reader.ProblemList);
+					}
+					else
+					{
+						var application = reader.VolunteerApplication;
+						// TODO Save the application to the database.
+					}
+					
+				}
+			}
+			
+			// TODO List files that failed to import.
+			Console.WriteLine("The following files could not be imported for the reasons stated.");
+			Console.WriteLine("Please correct problems and try again");
+			
+			foreach(var item in failures)
+			{
+				Console.WriteLine("File: " + item.Key);
+				Console.WriteLine("Problems:");
+				foreach(var problem in item.Value) Console.WriteLine(" * " + problem);
+			}
+			
+			Console.WriteLine("Process complete.");
+			
+			Console.Write("Press any key to close...");
 			Console.ReadKey(true);
 		}
 	}
---
+++ src/Library/S82Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RbcVolunteerApplications.Library
+{
+	public class S82Reader
+	{
+		public S82Reader(string filePath)
+		{
+			this.ReadS82File(filePath);
+		}
+		
+		public bool ReadSuccessful { get; set; }
+		
+		public List<string> ProblemList { get; set; }
+		
+		public VolunteerApplication VolunteerApplication { get; set; }
+		
+		private void ReadS82File(string filePath)
+		{
+			this.ReadSuccessful = true;
+			this.ProblemList = new List<string>();
+			
+			// When there is a problem, do this:
+			this.ReadSuccessful = false;
+			this.ProblemList.Add("Could not read BLA field. Please correct and try again.");
+		}
+	}
+}
