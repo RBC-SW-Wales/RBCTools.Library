c0fa071ca3574b19ec5be664ec7ec8563f689fce
Phil Baines <phil.baines@wubbleyew.com>
2013-10-13 18:49:49 +0100

    More ConsoleX features, new WriteTitle

--- src/Importer/ConsoleX.cs
+++ src/Importer/ConsoleX.cs
@@ -4,15 +4,26 @@ namespace RbcVolunteerApplications.Importer
 {
 	public static class ConsoleX
 	{
+		public static void WriteTitle(string intro)
+		{
+			Console.WriteLine(" ");
+			Console.WriteLine(" ");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("    " + intro);
+			Console.Write(new string('-', Console.WindowWidth));
+			Console.WriteLine(" ");
+			Console.ResetColor();
+		}
+		
 		public static void WriteIntro(string intro)
 		{
 			Console.WriteLine(" ");
-			Console.Write(new string('-', Console.WindowWidth));
-			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(" ");
+			Console.ForegroundColor = ConsoleColor.Blue;
 			Console.WriteLine("    " + intro);
-			Console.ResetColor();
 			Console.Write(new string('-', Console.WindowWidth));
 			Console.WriteLine(" ");
+			Console.ResetColor();
 		}
 		
 		public static void WriteLine(string text)
