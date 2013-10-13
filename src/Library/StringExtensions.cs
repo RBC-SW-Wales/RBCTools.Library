184a929f53b907300087dd0cbc1fb824f3d54837
Phil Baines <phil.baines@wubbleyew.com>
2013-10-13 20:40:25 +0100

    Adding StringExtensions.cs for TrimInnerWhitespace

---
+++ src/Library/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace RbcVolunteerApplications.Library
+{
+	/// <summary>
+	/// Extension methods for string
+	/// </summary>
+	public static class StringExtensions
+	{
+		public static string TrimInnerWhitespace(this string source)
+		{
+			while(source.Contains("  "))
+				source = source.Replace("  ", " ");
+			
+			return source;
+		}
+	}
+}
