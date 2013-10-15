8323ecd2b65f818e2c4bc8714288b7153183ac2c
Phil Baines <phil.baines@wubbleyew.com>
2013-10-15 01:08:35 +0100

    Add testing QueryDepartments command

---
+++ src/Importer/Commands/QueryDepartments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using RbcVolunteerApplications.Library;
+using RbcVolunteerApplications.Library.Database;
+
+namespace RbcVolunteerApplications.Importer.Commands
+{
+	public class QueryDepartments : CommandBase
+	{
+		public QueryDepartments()
+		{
+			base.Slug = "query-departments";
+			base.Description = "Query the Departments in the database";
+		}
+		
+		public override void Run()
+		{
+			var table = Departments.GetDepartmentsTable();
+			ConsoleX.WriteDataTable(table, 30);
+		}
+	}
+}
---
+++ src/Library/Database/Departments.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Data;
+
+namespace RbcVolunteerApplications.Library.Database
+{
+	public static class Departments
+	{
+		public static DataTable GetDepartmentsTable()
+		{
+			var connector = new Connector("SELECT ID, Trade " +
+			                              "FROM Trades " +
+			                              "ORDER BY Trade");
+			DataTable table = connector.ExecuteDataTable();
+			connector = null;
+			return table;
+		}
+	}
+}
