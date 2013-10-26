c3a44648a55c39ccfacdaf941c0037be4143c135
Phil Baines <phil.baines@wubbleyew.com>
2013-10-26 18:22:03 +0100

    Add build scripts

---
+++ build.cmd
@@ -0,0 +1,16 @@
+
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Debug
+)
+
+set msbuildPath= %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild
+
+echo.
+echo ** Building the solution **
+echo.
+
+%msbuildPath% src\RbcTools.sln
+
+pause
