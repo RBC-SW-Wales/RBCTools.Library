7f66da2ef34084ad20265f6e017de4d10f721f20
Phil Baines <phil.baines@wubbleyew.com>
2014-04-05 19:11:31 +0100

    Amend "AccessFileHelper" to use new Library class

--- src/Console/Helpers/AccessFileHelper.cs
+++ src/Console/Helpers/AccessFileHelper.cs
@@ -9,57 +9,48 @@ namespace RbcConsole.Helpers
 {
 	public static class AccessFileHelper
 	{
-		
-		private static string CorrectHash = "76dfc75952b6fb67da4dde043056a61190d8001a4a149816b64c17f0b244552e";
-		
 		public static void CheckForAccessFile(ConsoleX consoleX)
 		{
-			if(!Connector.AccessFileExists)
+			if(!AccessFileDownloader.AccessFileExists)
 			{
-				
 				consoleX.WriteIntro("Database requirements");
 				
 				string url = "";
-				string hash = "";
+				//string hash = "";
 				
 				consoleX.WriteWarning("It looks like you don't have the Access database file yet.");
 				consoleX.WriteLine("Don't worry, I can download it for you and save it to the correct place.");
+				
+				bool fileDownloaded = false;
 				
 				do
 				{
 					url = consoleX.WriteClipboardQuery("CORRECT URL");
 					url = url.Trim(); // Remove any whitespace that may have been copied by mistake.
-					hash = GetUrlHash(url);
 					
-					// consoleX.WriteLine(hash);
-					
-					if(hash != CorrectHash)
+					if(!AccessFileDownloader.IsUrlCorrect(url))
+					{
 						consoleX.WriteLine("Sorry, incorrect URL. Please try again.");
+					}
+					else
+					{
+						try
+						{
+							consoleX.WriteLine("Thanks! Downloading file now. Please wait...");
+							AccessFileDownloader.DownloadFile(url);
+							consoleX.WriteLine("OK, Done. You can now continue with your previous task.");
+							fileDownloaded = true;
+						} 
+						catch(Exception ex)
+						{
+							ExceptionHelper.HandleException(ex, consoleX);
+						}
+					}
 				}
-				while(hash != CorrectHash);
+				while(!fileDownloaded);
 				
-				consoleX.WriteLine("Thanks! Downloading file now. Please wait...");
-				
-				using(var client = new WebClient())
-				{
-					client.DownloadFile(url, Connector.AccessFilePath);
-				}
-				
-				consoleX.WriteLine("OK, Done. You can now continue with your previous task.");
 				consoleX.WriteHorizontalRule();
 			}
-		}
-		
-		private static string GetUrlHash(string url)
-		{
-			var sha256 = SHA256.Create();
-			var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(url));
-			StringBuilder builder = new StringBuilder(bytes.Length * 2);
-			foreach(var b in bytes)
-			{
-				builder.Append(b.ToString("x2"));
-			}
-			return builder.ToString();
 		}
 	}
 }
