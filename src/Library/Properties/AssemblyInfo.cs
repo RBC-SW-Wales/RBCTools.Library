9c3d616367ca30bc5facb66c2be427829d29ae05
Phil Baines <phil.baines@wubbleyew.com>
2014-11-23 15:49:23 +0000

    Updating version number

--- src/Library/Properties/AssemblyInfo.cs
+++ src/Library/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RBC Tools Library v1.1.3")]
+[assembly: AssemblyTitle("RBC Tools Library v1.1.4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RBC Tools Library v1.1.3")]
+[assembly: AssemblyProduct("RBC Tools Library v1.1.4")]
 [assembly: AssemblyCulture("")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.3.*")]
+[assembly: AssemblyVersion("1.1.4.*")]
